6e9e462a9bbe4dd58a4cf8e35d278b8932e6aabb
ltvu17 <133189798+ltvu17@users.noreply.github.com>
2024-06-24 14:24:48 +0700

    haha

--- src/data/Banner.ts
+++ src/data/Banner.ts
@@ -1,28 +1,28 @@
 export const banner = [
   {
     id: 1,
-    imgUrl: 'https://picsum.photos/200/300?random=1',
+    imgUrl: 'https://kingsport.vn/image/catalog/2024/T06/pc-gms.jpg',
     title: 'Airport Cabs',
   },
   {
     id: 2,
-    imgUrl: 'https://picsum.photos/200/300?random=3',
+    imgUrl: 'https://www.family.abbott/vn-vi/ensure/home/_jcr_content/root/container_559999535/image.coreimg.85.1280.jpeg/1716816312815/ens-h-homepage-20240527-img02.jpeg',
     title: 'Gift Cards',
   },
   {
     id: 3,
-    imgUrl: 'https://picsum.photos/200/300?random=5',
+    imgUrl: 'https://nhansamthinhphat.com/storage/category/hong-sam-han-quoc-thien-sam-kgc-thuong-hang-20240521-pimou3djk5yw.jpg',
     title: 'Hourly Stays',
   },
   {
     id: 4,
-    imgUrl: 'https://picsum.photos/200/300?random=7',
+    imgUrl: 'https://pos.nvncdn.com/d893b9-45589/bn/20230328_GzpQyr60.jpeg',
     title: 'Travel Insurance',
   },
-  {id: 5, imgUrl: 'https://picsum.photos/200/300?random=9', title: 'Forex'},
+  {id: 5, imgUrl: 'https://ongbaoi.com/hoavietphap/images/Anh-web/NEWw.jpg', title: 'Forex'},
   {
     id: 6,
-    imgUrl: 'https://picsum.photos/200/300?random=11',
+    imgUrl: 'https://theme.hstatic.net/200000491743/1000846299/14/collection_main_banner.jpg?v=708',
     title: 'HomeStays & Villas',
   },
 ];
