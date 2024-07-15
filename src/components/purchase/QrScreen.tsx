3e179871a05b5ca1c8c3421a04ef1d17d08804ca
qngvin <quangvinh11602@gmail.com>
2024-07-15 14:06:43 +0700

    fixx

--- src/components/purchase/QrScreen.tsx
+++ src/components/purchase/QrScreen.tsx
@@ -13,11 +13,11 @@ const QrScreen = () => {
   );
   console.log(info_order.totalCost);
   const bankId = 'MB';
-  const accountNo = '0888385759';
+  const accountNo = '0377899819';
   const template = 'compact2';
   const amount = info_order.totalCost + info_order.shipCost; // Replace with the actual amount
   const addInfo = 'Payment for services'; // Replace with the actual description
-  const accountName = 'Trần Quang Vinh'; // Replace with the actual account name
+  const accountName = 'Lê Văn Minh Nhật'; // Replace with the actual account name
 
   const imageUrl = `https://img.vietqr.io/image/${bankId}-${accountNo}-${template}.png?amount=${amount}&addInfo=${encodeURIComponent(
     addInfo,
